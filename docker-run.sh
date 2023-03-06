f2db6803ec3932135f48c59df0ea41a270cc6f79
Jakub Waller <jakub.waller@protonmail.com>
2023-03-05 20:51:15 -0400

    changed docker-run

--- docker-run.sh
+++ docker-run.sh
@@ -2,5 +2,5 @@
 
 docker stop probablefuturesbot
 docker rm probablefuturesbot
-docker run --restart always --name probablefuturesbot -d -v /home/ubuntu/probable-futures-bot/logs/:/probablefuturesbot/probablefuturesbot/logs probablefuturesbot/probablefuturesbot bash -c "cd /probablefuturesbot/probablefuturesbot && python3 -m probablefuturesbot"
+docker run --restart always --name probablefuturesbot -d -v /home/ubuntu/probable-futures-bot/logs/:/probablefuturesbot/logs probablefuturesbot/probablefuturesbot bash -c "cd /probablefuturesbot && python3 -m probablefuturesbot"
 docker logs -f probablefuturesbot
